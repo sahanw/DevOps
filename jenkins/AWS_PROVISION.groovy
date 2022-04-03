f94e8758af74607356ef1f502bd0545687a09f7b
Sahan W <sahan.wickramaarachchi@gmail.com>
2022-04-03 18:25:58 +0530

    tf pipeline
    Signed-off-by: Sahan W <sahan.wickramaarachchi@gmail.com>

--- jenkins/AWS_PROVISION.groovy
+++ jenkins/AWS_PROVISION.groovy
@@ -2,20 +2,39 @@ node {
     stage('Checkout') {
         checkout scm
     }
+    stages{
+        stage('Create AWS EC2 Instance') {
+            sh '''
+            sleep 10;
+            pwd
+            cp scripts/test.sh .
+            cat test.sh
+            aws ec2 describe-instances
+            '''
+            print "Hello World!\n"
+            print "What is your name? "    
+        }
 
-    stage('Testing Stage') {
-        sh '''
-        sleep 10;
-        pwd
-        cp scripts/test.sh .
-        cat test.sh
-        aws ec2 describe-instances
-        '''
-        print "Hello World!\n"
-        print "What is your name? "    
-    }
-
-    stage("Clean Workspace"){
-        cleanWs()
+        stage('Terraform Init') {
+            steps {
+                sh "terraform init -input=false"
+            }
+        }
+    
+        stage('Terraform Plan') {
+            steps {
+                sh "terraform plan -out=tfplan -input=false -var-file='dev.tfvars'"
+            }
+        }
+    
+        stage('Terraform Apply') {
+            steps {
+                input 'Apply Plan'
+                sh "terraform apply -input=false terraform/AWS_PROV.tf"
+            }
+        }
+            stage("Clean Workspace"){
+            cleanWs()
+        }
     }
 }
