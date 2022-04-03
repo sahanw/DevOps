f6dd8b9157bd6de64c60d3656f59a2bc20439050
Sahan W <sahan.wickramaarachchi@gmail.com>
2022-04-03 20:20:10 +0530

    tfs3
    Signed-off-by: Sahan W <sahan.wickramaarachchi@gmail.com>

--- terraform/S3_PROV.tf
+++ terraform/S3_PROV.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket_object" "file_upload" {
   bucket = "TF_STATE_SAHAN"
   source = "terraform.tfstate"
+  key    = "terraform.tfstate"
   etag = filemd5("terraform.tfstate")
 }
