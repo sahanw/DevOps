2cee018b9ce8ed76cc08877e32666f447f709e4d
Sahan W <sahan.wickramaarachchi@gmail.com>
2022-04-03 19:40:44 +0530

    tf
    Signed-off-by: Sahan W <sahan.wickramaarachchi@gmail.com>

--- terraform/AWS_PROV.tf
+++ terraform/AWS_PROV.tf
@@ -60,12 +60,7 @@ resource "aws_instance" "project-iac" {
   vpc_security_group_ids = [
     aws_security_group.project-iac-sg.id
   ]
-  root_block_device {
-    delete_on_termination = true
-    iops = 150
-    volume_size = 50
-    volume_type = "gp2"
-  }
+
   tags = {
     Name ="SERVER01"
     Environment = "DEV"
