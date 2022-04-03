05224e2aaa5f81baa43c16d5443397135e176e94
Sahan W <sahan.wickramaarachchi@gmail.com>
2022-04-03 17:39:22 +0530

    aws
    Signed-off-by: Sahan W <sahan.wickramaarachchi@gmail.com>

--- jenkins/AWS_PROVISION.groovy
+++ jenkins/AWS_PROVISION.groovy
@@ -9,9 +9,12 @@ node {
         pwd
         cp scripts/test.sh .
         cat test.sh
+        aws ec2 describe-instances
         '''
         print "Hello World!\n"
         print "What is your name? "
+
+        
     }
 
     stage("Clean Workspace"){
