3bc271040f11c196b85583634333c038bff8b6db
Sahan W <sahan.wickramaarachchi@gmail.com>
2022-04-03 19:01:41 +0530

    tf
    Signed-off-by: Sahan W <sahan.wickramaarachchi@gmail.com>

--- jenkins/AWS_PROVISION.groovy
+++ jenkins/AWS_PROVISION.groovy
@@ -1,35 +1,48 @@
 pipeline {
     agent any 
-    
-    stage('Checkout') {
-        checkout scm
-    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout scm
+            }
+        }
 
         stage('Create AWS EC2 Instance') {
-            sh '''
-            pwd
-            cp scripts/test.sh .
-            cat test.sh
-            aws ec2 describe-instances
-            '''   
+            steps {
+                sh '''
+                pwd
+                cp scripts/test.sh .
+                cat test.sh
+                aws ec2 describe-instances
+                '''   
+            }
         }
 
         stage('Terraform Init') {
+            steps {
                 sh '''
                 cd terraform
                 terraform init -input=false
                 '''
+            }
         }
     
         stage('Terraform Plan') {
+            steps {
                 sh "terraform plan"
+            }
         }
     
         stage('Terraform Apply') {
+            steps {
                 input 'Apply Plan'
                 sh "terraform apply -input=false terraform/AWS_PROV.tf -autoapprove"
+            }
         }
-            stage("Clean Workspace"){
-            cleanWs()
+        stage("Clean Workspace"){
+            steps {
+                cleanWs()
+            }
         }
 }
