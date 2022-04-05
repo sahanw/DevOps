f475eac96175827212da07f6c6827edd61a6e2a3
Sahan W <sahan.wickramaarachchi@gmail.com>
2022-04-05 21:30:01 +0530

    tf
    Signed-off-by: Sahan W <sahan.wickramaarachchi@gmail.com>

--- jenkins/AWS_PROVISION.groovy
+++ jenkins/AWS_PROVISION.groovy
@@ -41,7 +41,9 @@ pipeline {
             steps {
                 sh """
                 cd terraform
-                terraform apply -auto-approve -var=sg_name=SG_NAME_SAHAN -var=server_Name=SERVER"${params.Env_ID}"
+                terraform apply -auto-approve 
+                    -var=sg_name=SG_NAME_SAHAN 
+                    -var=server_Name=SERVER"${params.Env_ID}"
                 ls -l
                 aws s3 cp terraform.tfstate s3://tfst
                 """
