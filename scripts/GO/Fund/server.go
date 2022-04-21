93ed1965c5cecb109fc93217101c697716a24c83
Sahan W <sahan.wickramaarachchi@gmail.com>
2022-04-21 06:33:58 +0530

    go
    Signed-off-by: Sahan W <sahan.wickramaarachchi@gmail.com>

---
+++ scripts/GO/Fund/server.go
@@ -0,0 +1,28 @@
+package funding
+
+type FundServer struct {
+	Commands chan interface{}
+	fund     Fund
+}
+
+func NewFundServer(initialBalance int) *FundServer {
+	server := &FundServer{
+		// make() creates builtins like channels, maps, and slices
+		Commands: make(chan interface{}),
+		fund:     NewFund(initialBalance),
+	}
+
+	// Spawn off the server's main loop immediately
+	go server.loop()
+	return server
+}
+
+func (s *FundServer) loop() {
+	// The built-in "range" clause can iterate over channels,
+	// amongst other things
+	for command := range s.Commands {
+
+		// Handle the command
+
+	}
+}
