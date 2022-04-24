37716a676f9aee850784ec6a8aad7e836fc884f7
Sahan W <sahan.wickramaarachchi@gmail.com>
2022-04-24 17:59:40 +0530

    Script to print permutation of a string
    Signed-off-by: Sahan W <sahan.wickramaarachchi@gmail.com>

---
+++ scripts/Python/permutation.py
@@ -0,0 +1,20 @@
+# Function to print permutations of string
+
+def toString(List):
+    return ''.join(List)
+
+def permute(arr, l, r): #String, Starting Index, Length, 
+    if l == r:
+        print(toString(a))
+    else:
+        for i in range(l, r):
+            arr[l], arr[i] = arr[i], arr[l]
+            permute(arr, l+1, r)
+            arr[l], arr[i] = arr[i], arr[l]  # backtrack
+
+# Driver program to test the above function
+string = "4321"
+n = len(string)
+a = list(string)
+
+permute(a, 0, n)
