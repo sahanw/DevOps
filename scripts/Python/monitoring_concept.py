1acc8a21c0f3c4b01de8fd3216a388f5007b3c84
Sahan W <sahan.wickramaarachchi@gmail.com>
2022-06-14 12:08:53 +0530

    Monitoring Conceptual Script
    Signed-off-by: Sahan W <sahan.wickramaarachchi@gmail.com>

---
+++ scripts/Python/monitoring_concept.py
@@ -0,0 +1,25 @@
+# Given is an array of latency values, a time window and threshold. 
+# This script will output the time we should throw an alert 
+# The point in time is represented by the index of the latency value when the threshold is crossed for the given time span 
+
+#Sample Input:
+#latency = [1,2,4,4,3,7,8,9,6,7]
+#window = 3
+#threshold = 5 
+#Expected output: 7 
+
+latency = [1,2,4,4,3,7,8,0,9,0,6,7]
+
+window = 3 
+threshold = 5 
+alertCount=0
+
+for l in range(0, len(latency)-window+1):
+    for i in range(l, l+window):
+        if(latency[i]>=threshold):
+            alertCount=alertCount+1
+    if(alertCount>=window):
+            print(i)
+            break
+    else: 
+        alertCount=0
