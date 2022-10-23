73665c1581e8407ce2554b238574cae58287da15
Sahan Wickramaarachchi <sahan.wickramaarachchi@gmail.com>
2022-10-23 20:09:35 +0530

    #
    Signed-off-by: Sahan Wickramaarachchi <sahan.wickramaarachchi@gmail.com>

--- jenkins/DOCKER_AATHAL.groovy
+++ jenkins/DOCKER_AATHAL.groovy
@@ -1,24 +1,32 @@
 pipeline {
     agent any 
     stages {
+        options 
+        {
+            ansiColor('xterm')
+        }
+
         stage('Checkout SCM') 
         {
             steps {
                 checkout scm
             }
         }
+
         stage('Build') 
         { 
             steps {
                 // 
             }
         }
+
         stage('Test') 
         { 
             steps {
                 // 
             }
         }
+        
         stage('Deploy') 
         { 
             steps {
