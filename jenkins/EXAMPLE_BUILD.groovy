811add1b7d7d84f40b45d6cf7d0f54d321b802a6
Sahan Wickramaarachchi <sahan.wickramaarachchi@gmail.com>
2023-03-27 21:35:25 +0100

    dummy
    Signed-off-by: Sahan Wickramaarachchi <sahan.wickramaarachchi@gmail.com>

--- jenkins/EXAMPLE_BUILD.groovy
+++ jenkins/EXAMPLE_BUILD.groovy
@@ -1,10 +1,42 @@
 pipeline {
     agent none
+
     stages {
-        stage('Hello World') {
+        stage('Checkout SCM') 
+        {
             steps {
-                echo "Hello, World!"
+                checkout scm
+            }
+        }
+        stage('Copying the Artifacts') {
+            steps {
+                echo "Copying Completed"
+            }
+        }
+        stage('Backing Up Existing Configs') 
+        { 
+            steps {
+                sh "date"
+                sh "echo Backup Completed"
+                sh "sleep 10" 
+            }
+        }
+        stage('Build SWIFT Alliance Access') 
+        { 
+            steps {
+                sh "date"
+                sh "sleep 10"
+                sh "echo Building Completed" 
+            }
+        }
+        stage('Restart SWIFT Alliance Access') 
+        { 
+            steps {
+                sh "df -h"
+                sh "sleep 10"
+                sh "echo Restarting Completed" 
             }
         }
     }
+
 }
